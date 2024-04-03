b434fbbddd1257d41f6fc64d4faaddf86aee8fea
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-04-03 20:21:30 -0300

    Added EscopoFuncao.js

---
+++ EscopoFuncao.js
@@ -0,0 +1,8 @@
+function informacao() {
+    const nome = "Pedro";
+    const idade = 20;
+    console.log(`O nome do usuário é ${nome} e tem ${idade} anos.`)
+}
+informacao()  // Funciona
+
+// console.log(`${nome}, ${idade}`) -- as variaveis não podem ser acessadas de fora da função
